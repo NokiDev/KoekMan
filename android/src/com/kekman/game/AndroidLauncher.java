df89f4942dd1f2d2c3bfaa7160ec22f98aab3665
aria.groult@outlook.fr <aria.groult@outlook.fr>
2017-02-27 11:16:02 +0100

    first commitééé

---
+++ android/src/com/kekman/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.kekman.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kekman.game.KekMan;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new KekMan(), config);
+	}
+}
